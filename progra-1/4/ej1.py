6bd66dbbacba7bd0cd23d911ac07f2485650bbf9
enzocazenave <encaze24@hotmail.com>
2022-09-13 09:16:50 -0300

    Changes

--- progra-1/4/ej1.py
+++ progra-1/4/ej1.py
@@ -4,7 +4,7 @@
     verificar su funcionamiento.
 """
 
-def es_capicua(str):
+def es_capicua_1(str):
     cont = 0
     cont_invertido = len(str) - 1
     capicua = False
@@ -20,9 +20,25 @@ def es_capicua(str):
     return capicua
 
 
+def es_capicua_2(str):
+    capicua = True
+
+    for i in range(len(str)):
+        if str[i] != str[-i-1]:
+            capicua = False
+            break
+
+    return capicua
+
+
 str = input('\nIngrese una palabra para verificar si es capicua: ')
 
-if es_capicua(str):
-    print('\nLa palabra (', str,') es capicua')
+if es_capicua_1(str):
+    print('\n[WHILE] La palabra (', str,') es capicua')
 else:
-    print('\nLa palabra (', str,') no es capicua')
+    print('\n[WHILE] La palabra (', str,') no es capicua')
+
+if es_capicua_2(str):
+    print('\n[FOR] La palabra (', str,') es capicua')
+else:
+    print('\n[FOR] La palabra (', str,') no es capicua')
